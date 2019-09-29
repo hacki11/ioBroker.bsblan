1a09b8f326529a0a4ea21c26dbf30b7f4a15d768
Juergen Schmid <jur.schmid@gmail.com>
2019-09-29 16:41:38 +0200

    initial commit of created adapter

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "bsblan adapter settings": {
+        "en": "Adapter settings for bsblan",
+        "de": "Adaptereinstellungen für bsblan",
+        "ru": "Настройки адаптера для bsblan",
+        "pt": "Configurações do adaptador para bsblan",
+        "nl": "Adapterinstellingen voor bsblan",
+        "fr": "Paramètres d'adaptateur pour bsblan",
+        "it": "Impostazioni dell'adattatore per bsblan",
+        "es": "Ajustes del adaptador para bsblan",
+        "pl": "Ustawienia adaptera dla bsblan",
+        "zh-cn": "bsblan的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
