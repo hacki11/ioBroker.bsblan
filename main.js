eb905c66928b1e77444d76f3919c2c442a3a90e9
Juergen Schmid <jur.schmid@gmail.com>
2019-11-06 23:13:07 +0100

    - fix review findings - support compact mode

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-const BSB = require('./lib/bsb');
+const BSB = require("./lib/bsb");
 
 class Bsblan extends utils.Adapter {
 
@@ -20,9 +20,6 @@ class Bsblan extends utils.Adapter {
             name: "bsblan",
         });
         this.on("ready", this.onReady.bind(this));
-        this.on("objectChange", this.onObjectChange.bind(this));
-        this.on("stateChange", this.onStateChange.bind(this));
-        // this.on("message", this.onMessage.bind(this));
         this.on("unload", this.onUnload.bind(this));
     }
 
@@ -248,74 +245,21 @@ class Bsblan extends utils.Adapter {
      */
     onUnload(callback) {
         try {
+            clearTimeout(this.timer);
             this.log.info("cleaned everything up...");
             callback();
         } catch (e) {
             callback();
         }
     }
-
-    /**
-     * Is called if a subscribed object changes
-     * @param {string} id
-     * @param {ioBroker.Object | null | undefined} obj
-     */
-    onObjectChange(id, obj) {
-        if (obj) {
-            // The object was changed
-            // this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-        } else {
-            // The object was deleted
-            this.log.info(`object ${id} deleted`);
-        }
-    }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            // this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
-        }
-    }
-
-// /**
-//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-//  * Using this method requires "common.message" property to be set to true in io-package.json
-//  * @param {ioBroker.Message} obj
-//  */
-// onMessage(obj) {
-// 	if (typeof obj === "object" && obj.message) {
-// 		if (obj.command === "send") {
-// 			// e.g. send email or pushover or whatever
-// 			this.log.info("send command");
-
-// 			// Send response in callback if required
-// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-// 		}
-// 	}
-// }
-
 }
 
 // @ts-ignore parent is a valid property on module
-if (module
-
-    .parent
-) {
+if (module && module.parent) {
     // Export the constructor in compact mode
-    /**
-     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
-     */
-    module
-        .exports = (options) => new Bsblan(options);
+    module.exports = (options) => new Bsblan(options);
 } else {
     // otherwise start the instance directly
+    //this.log.info("running in normal mode");
     new Bsblan();
 }
