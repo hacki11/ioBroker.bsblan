bcf2c5a6307a10fb1e20436b1979ebaf34bd2138
Juergen Schmid <15319906+hacki11@users.noreply.github.com>
2025-04-19 22:47:56 +0200

    migrate to eslint and prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+// iobroker prettier configuration file
+import prettierConfig from "@iobroker/eslint-config/prettier.config.mjs";
+
+export default {
+    ...prettierConfig,
+    singleQuote: false,
+    allowParens: "avoid",
+};
