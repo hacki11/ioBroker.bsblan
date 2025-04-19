9c024abc9ac48e7431a150b537e81e7e29c7e586
Jürgen Schmid <15319906+hacki11@users.noreply.github.com>
2025-04-19 23:31:31 +0200

    postcreate_ext.sh aktualisieren

--- .devcontainer/scripts/postcreate_ext.sh
+++ .devcontainer/scripts/postcreate_ext.sh
@@ -4,4 +4,4 @@ set -e
 
 iob object set system.adapter.bsblan.0 native.host=192.168.0.14
 iob object set system.adapter.bsblan.0 native.user=admin
-iob object set system.adapter.bsblan.0 native.password=mirinda
+iob object set system.adapter.bsblan.0 native.password=
